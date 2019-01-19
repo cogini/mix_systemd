e5c6c546c86c432f0c10fac9d69dcb8b4dc5d12d
Jake Morrison <jake@cogini.com>
2019-01-19 12:30:18 +0800

    Disable dialyzer

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule MixSystemd.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 0.5.1", only: [:dev, :test], runtime: false},
+      # {:dialyxir, "~> 0.5.1", only: [:dev, :test], runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
