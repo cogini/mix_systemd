6661f9ed81ac06d0073d69f7169ed939741d003b
Jake Morrison <jake@cogini.com>
2019-06-17 15:35:16 +0800

    Set homepage_url

--- mix.exs
+++ mix.exs
@@ -11,6 +11,7 @@ defmodule MixSystemd.MixProject do
       description: description(),
       package: package(),
       source_url: "https://github.com/cogini/mix_systemd",
+      homepage_url: "https://github.com/cogini/mix_systemd",
       dialyzer: [
         plt_add_apps: [:mix, :eex]
         # plt_add_deps: true,
