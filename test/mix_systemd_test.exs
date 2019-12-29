649aaca15a9de102b389c5ed3415de02084c4c82
Jake Morrison <jake@cogini.com>
2019-12-29 13:35:34 +0800

    Test

--- test/mix_systemd_test.exs
+++ test/mix_systemd_test.exs
@@ -1,6 +1,22 @@
 defmodule MixSystemdTest do
   use ExUnit.Case, async: true
 
+  test "create_config/2" do
+    mix_config = Mix.Project.config()
+    user_config = [base_dir: "/opt"]
+    cfg = Mix.Tasks.Systemd.create_config(mix_config, user_config)
+
+    assert cfg[:app_name] == :mix_systemd
+    assert cfg[:ext_name] == "mix-systemd"
+    assert cfg[:module_name] == "MixSystemd"
+    assert cfg[:deploy_dir] == "/opt/mix-systemd"
+    assert cfg[:releases_dir] == "/opt/mix-systemd/releases"
+    assert cfg[:configuration_dir] == "/etc/mix-systemd"
+    assert cfg[:pid_file] == "/run/mix-systemd/mix_systemd.pid"
+    assert cfg[:working_dir] == "/opt/mix-systemd/current"
+    assert cfg[:start_command] == "start"
+  end
+
   describe "ensure_trailing_space/1" do
     test "nil returns empty string" do
       assert Mix.Tasks.Systemd.ensure_trailing_space(nil) == ""
