5895c4d278e9d2e7962fb11e70375933492f70bf
Jake Morrison <jake@cogini.com>
2024-05-29 16:45:47 -0500

    Add static analysis tools

--- mix.exs
+++ mix.exs
@@ -11,11 +11,7 @@ defmodule MixSystemd.MixProject do
       elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      description: description(),
-      package: package(),
-      source_url: @github,
-      homepage_url: @github,
-      docs: docs(),
+      aliases: aliases(),
       dialyzer: [
         plt_add_apps: [:mix, :eex]
         # plt_add_deps: true,
@@ -23,22 +19,40 @@ defmodule MixSystemd.MixProject do
         # flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs],
         # ignore_warnings: "dialyzer.ignore-warnings"
       ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.lcov": :test,
+        quality: :test,
+        "quality.ci": :test
+      ],
+      description: description(),
+      package: package(),
+      source_url: @github,
+      homepage_url: @github,
+      docs: docs(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger, :eex]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.32.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.18.0", only: [:dev, :test], runtime: false},
+      {:junit_formatter, "~> 3.3", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false}
+      # {:styler, "~> 0.10.0", only: [:dev, :test], runtime: false},
     ]
   end
 
@@ -48,18 +62,59 @@ defmodule MixSystemd.MixProject do
 
   defp package do
     [
+      description: description(),
       maintainers: ["Jake Morrison"],
-      licenses: ["Apache 2.0"],
-      links: %{"GitHub" => @github}
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @github,
+        "Changelog" =>
+          "#{@github}/blob/#{@version}/CHANGELOG.md##{String.replace(@version, ".", "")}"
+      }
     ]
   end
 
   defp docs do
     [
+      main: "readme",
       source_url: @github,
-      extras: ["README.md", "CHANGELOG.md"],
+      source_ref: @version,
+      extras: [
+        "README.md",
+        "CHANGELOG.md": [title: "Changelog"],
+        "LICENSE.md": [title: "License (Apache-2.0)"],
+        "CONTRIBUTING.md": [title: "Contributing"],
+        "CODE_OF_CONDUCT.md": [title: "Code of Conduct"]
+      ],
       # api_reference: false,
       source_url_pattern: "#{@github}/blob/master/%{path}#L%{line}"
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get"],
+      quality: [
+        "test",
+        "format --check-formatted",
+        # "credo",
+        "credo --mute-exit-status",
+        # mix deps.clean --unlock --unused
+        "deps.unlock --check-unused",
+        # mix deps.update
+        # "hex.outdated",
+        # "hex.audit",
+        "deps.audit",
+        "dialyzer --quiet-with-result"
+      ],
+      "quality.ci": [
+        "format --check-formatted",
+        "deps.unlock --check-unused",
+        # "hex.outdated",
+        "hex.audit",
+        "deps.audit",
+        "credo",
+        "dialyzer --quiet-with-result"
+      ]
     ]
   end
 end
